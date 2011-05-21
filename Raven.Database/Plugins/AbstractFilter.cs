f72480ace278203034abea2578715b65185ec807
fschwiet <fschwiet@gmail.com>
2011-05-20 19:30:22 -0700

    this will work better

---
+++ Raven.Database/Plugins/AbstractFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lucene.Net.Search;
+using Raven.Json.Linq;
+
+namespace Raven.Database.Plugins
+{
+    public abstract class AbstractFilter : IRequiresDocumentDatabaseInitialization
+    {
+        public abstract void Initialize(DocumentDatabase database);
+        public abstract string GetName();
+        public abstract Filter Create(RavenJArray parameters);
+    }
+}
