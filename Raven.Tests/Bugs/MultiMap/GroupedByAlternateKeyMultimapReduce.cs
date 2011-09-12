597c8630360e6a28331e9f738ac5e753807f4a46
fschwiet <fschwiet@gmail.com>
2011-09-12 12:53:17 -0700

    hmm trying multimap with computed grouping

---
+++ Raven.Tests/Bugs/MultiMap/GroupedByAlternateKeyMultimapReduce.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Raven.Abstractions.Indexing;
+using Raven.Client.Indexes;
+using Xunit;
+
+namespace Raven.Tests.Bugs.MultiMap
+{
+	public class GroupedByAlternateKeyMultimapReduce : MultiMapReduce
+	{
+		public class Post : MultiMapReduce.Post
+		{
+			public string PostersName;
+		}
+
+		public class LastLetterPostingStats
+		{
+			public string[] UserNames { get; set; }
+			public int PostCount { get; set; }
+			public string FirstLetterOfName { get; set; }
+		}
+
+		public class PostCountsByFirstLetter_WithNames : AbstractMultiMapIndexCreationTask<LastLetterPostingStats>
+		{
+			public PostCountsByFirstLetter_WithNames()
+			{
+				AddMap<User>(users => from user in users
+									  select new
+									  {
+										  UserNames = new string[] {user.Name},
+										  PostCount = 0,
+										  FirstLetterOfName = string.IsNullOrEmpty(user.Name) ? " " : user.Name.Substring(0, 1)
+									  });
+
+				AddMap<Post>(posts => from post in posts
+									  select new
+									  {
+										  UserNames = new string[0],
+										  PostCount = 1,
+										  FirstLetterOfName = string.IsNullOrEmpty(post.PostersName) ? " " : post.PostersName.Substring(0, 1)
+									  });
+
+				Reduce = results => from result in results
+									group result by result.FirstLetterOfName
+										into g
+										select new
+										{
+											UserNames = g.SelectMany(r => r.UserNames).Distinct().ToArray(),
+											PostCount = g.Sum(x => x.PostCount),
+											FirstLetterOfName = g.Key
+										};
+
+				Index(x => x.FirstLetterOfName, FieldIndexing.Analyzed);
+			}
+		}
+
+
+		[Fact]
+		public void CanGroupByFirstLetterOfName()
+		{
+			using (var store = NewDocumentStore())
+			{
+				new PostCountsByFirstLetter_WithNames().Execute(store);
+
+				using (var session = store.OpenSession())
+				{
+					var userAyende = new User
+					{
+						Name = "Ayende Rahien"
+					};
+
+					session.Store(userAyende);
+
+					session.Store(new User()
+					{
+						Name = "Another User"
+					});
+
+					session.Store(new User()
+					{
+						Name = "Billionth"
+					});
+
+					for (int i = 0; i < 5; i++)
+					{
+						session.Store(new Post
+						{
+							AuthorId = userAyende.Id,
+							Title = "blah",
+							PostersName = userAyende.Name
+						});
+					}
+
+					session.SaveChanges();
+				}
+
+				using (var session = store.OpenSession())
+				{
+					var ups = session.Query<LastLetterPostingStats, PostCountsByFirstLetter_WithNames>()
+						.Customize(x => x.WaitForNonStaleResults())
+						.Where(x => x.FirstLetterOfName == "a")
+						.Single();
+
+					Assert.True(ups.FirstLetterOfName == "a");
+					Assert.True(ups.UserNames.Contains("Ayende Rahien"));
+					Assert.True(ups.UserNames.Contains("Another User"));
+					Assert.True(!ups.UserNames.Contains("Another User"));
+					Assert.Equal(5, ups.PostCount);
+				}
+			}
+		}
+	}
+}
--- Raven.Tests/Bugs/MultiMap/MultiMapReduce.cs
+++ Raven.Tests/Bugs/MultiMap/MultiMapReduce.cs
@@ -132,7 +132,64 @@ namespace Raven.Tests.Bugs.MultiMap
 					Assert.Equal("Ayende Rahien", ups[0].UserName);
 				}
 			}
+		}
 
+		[Fact]
+		public void CanQueryByFirstLetterOfLastName()
+		{
+			using (var store = NewDocumentStore())
+			{
+				new PostCountsByUser_WithName().Execute(store);
+
+				using (var session = store.OpenSession())
+				{
+					var userAyende = new User
+					{
+						Name = "Ayende Rahien"
+					};
+
+					session.Store(userAyende);
+
+					session.Store(new User()
+					{
+						Name = "Another User"
+					});
+
+					session.Store(new User()
+					{
+						Name = "Billionth"  
+					});
+
+					for (int i = 0; i < 5; i++)
+					{
+						session.Store(new Post
+						{
+							AuthorId = userAyende.Id,
+							Title = "blah"
+						});
+					}
+
+					session.SaveChanges();
+				}
+
+				using (var session = store.OpenSession())
+				{
+					var ups = session.Query<UserPostingStats, PostCountsByUser_WithName>()
+						.Customize(x => x.WaitForNonStaleResults())
+						.Where(x => x.FirstLetterOfLastName == "a")
+						.ToList();
+
+					var firstletters = session.Advanced.DatabaseCommands.GetTerms(new PostCountsByUser_WithName().IndexName, "FirstLetterOfLastName", null, 1000).ToArray();
+					var names = session.Advanced.DatabaseCommands.GetTerms(new PostCountsByUser_WithName().IndexName, "UserName", null, 1000).ToArray();
+
+					Assert.Contains("b", firstletters);
+					Assert.Contains("a", firstletters);  // fails here
+
+					Assert.True(ups.All(u => u.FirstLetterOfLastName == "a"));
+					Assert.True(ups.Any(u => u.UserName == "Ayende Rahien"));
+					Assert.True(ups.Any(u => u.UserName == "Another User"));
+				}
+			}
 		}
 
 
@@ -154,6 +211,7 @@ namespace Raven.Tests.Bugs.MultiMap
 			public string UserName { get; set; }
 			public string UserId { get; set; }
 			public int PostCount { get; set; }
+			public string FirstLetterOfLastName { get; set; }
 		}
 
 		public class PostCountsByUser_WithName : AbstractMultiMapIndexCreationTask<UserPostingStats>
@@ -165,7 +223,8 @@ namespace Raven.Tests.Bugs.MultiMap
 									  {
 										  UserId = user.Id,
 										  UserName = user.Name,
-										  PostCount = 0
+										  PostCount = 0,
+										  FirstLetterOfLastName = string.IsNullOrEmpty(user.Name) ? " " : user.Name.Substring(0,1)
 									  });
 
 				AddMap<Post>(posts => from post in posts
@@ -173,20 +232,23 @@ namespace Raven.Tests.Bugs.MultiMap
 									  {
 										  UserId = post.AuthorId,
 										  UserName = (string)null,
-										  PostCount = 1
+										  PostCount = 1,
+										  FirstLetterOfLastName = (string)null
 									  });
 
 				Reduce = results => from result in results
-				                    group result by result.UserId
-				                    into g
-				                    select new
-				                    {
-				                    	UserId = g.Key,
-				                    	UserName = g.Select(x => x.UserName).Where(x => x != null).FirstOrDefault(),
-				                    	PostCount = g.Sum(x => x.PostCount)
-				                    };
+									group result by result.UserId
+									into g
+									select new
+									{
+										UserId = g.Key,
+										UserName = g.Select(x => x.UserName).Where(x => x != null).FirstOrDefault(),
+										PostCount = g.Sum(x => x.PostCount),
+										FirstLetterOfLastName = g.Select(x => x.FirstLetterOfLastName).Where(x => x != null).FirstOrDefault()
+									};
 
 				Index(x=>x.UserName, FieldIndexing.Analyzed);
+				Index(x => x.FirstLetterOfLastName, FieldIndexing.Analyzed);
 			}
 		}
 	}
